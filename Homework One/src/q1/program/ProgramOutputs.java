ad8fe7654fcbb5d4d4eecda8dd1cba2bcbe72fc9
danthomps1999 <dthomp69@emich.edu>
2022-02-01 13:22:03 -0500

    started writing structure

--- Homework One/src/q1/program/ProgramOutputs.java
+++ Homework One/src/q1/program/ProgramOutputs.java
@@ -1,12 +1,22 @@
 package q1.program;
 
+import java.io.BufferedWriter;
+
 public class ProgramOutputs {
 
+	private InfoPasser outputs;
+
 	public void writeOutputs(InfoPasser outputs) {
+		this.outputs = outputs;
+
 		System.out.println("RunTime (Nanoseconds): " + outputs.getRunTime());
 		System.out.println("RunTime (Milliseconds): " + outputs.getRunTime() / 1000000);
 		System.out.println("Swaps: " + outputs.getSwaps());
 		System.out.println("BoardsSearched: " + outputs.getBoardsSearched());
+	}
+
+	public void writeToFile() {
+//		BufferedWriter writer = new BufferedWriter();
 	}
 
 }
