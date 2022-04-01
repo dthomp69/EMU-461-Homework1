ffffa36a7e04697f8331c230320cbeeccfddb25a
danthomps1999 <dthomp69@emich.edu>
2022-03-31 21:00:20 -0400

    Prepping for q4, and updated the wrong record in q2

---
+++ Homework Three/src/mannicamKnapsack/KnapsackTester.java
@@ -0,0 +1,31 @@
+package mannicamKnapsack;
+
+/*****************************************************************************/
+
+//Program tests knapsack solver
+public class KnapsackTester
+{
+    //main method
+    public static void main(String[] args)
+    {
+         //knapsack data
+         int numberItems = 5;
+         int maximumWeight = 40;
+         int[][] table = {{15, 600},
+                          {12, 200},
+                          {10, 300},
+                          {14, 500},
+                          {18, 700}};
+
+         //create knapsack solver
+         Knapsack k = new Knapsack(table, numberItems, maximumWeight);
+
+         //set parameters of genetic algorithm
+         k.setParameters(50, 5, 1000, 0.5, 0.1, 4329);
+
+         //find optimal solution
+         k.solve();
+    }
+}
+
+/*****************************************************************************/
